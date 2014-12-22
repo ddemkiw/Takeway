91b92d417de2b7fe722cbac7ce10fa12ca026949
Danielle Demkiw <ddemkiw@gmail.com>
2014-12-22 12:05:20 +0000

    Adding Dish Class and Tests

---
+++ lib/dish.rb
@@ -0,0 +1,25 @@
+class Dish
+
+attr_accessor :price, :name
+
+  def initialize(options = {})
+    self.price = options.fetch(:price, price)
+    self.name = options.fetch(:name, name) 
+  end
+
+
+  def self.peking_duck
+    new(:price => 3.50, :name => "Peking Duck")
+  end
+
+  def self.chow_mein
+    new(:price => 4.00, :name => "Chow Mein")
+  end
+
+  def self.crispy_beef
+    new(:price => 5.00, :name => "Crispy Beef")
+  end
+
+end
+
+
---
+++ spec/dish_spec.rb
@@ -0,0 +1,35 @@
+require 'dish'
+
+describe Dish do
+
+  let (:dish) {Dish.new(:price => 3.50, :name => "duck")}
+  
+  it 'should be initialized with a price' do
+    expect(dish.price).to eq(3.50)  
+  end
+
+  it 'should be initialized with a name' do
+    expect(dish.name).to eq("duck")
+  end
+ 
+   context 'individual dishes' do 
+      
+      it 'should initialized Chow Mein with a price of 4.00' do
+      expect(Dish.chow_mein.price).to eq(4.00)
+      end
+
+      it 'should initialize Chow Mein with a name' do
+        expect(Dish.chow_mein.name).to eq("Chow Mein")
+      end
+
+      it 'should initialize Crispy Beef with a name' do
+        expect(Dish.crispy_beef.name).to eq("Crispy Beef")
+      end
+
+      it 'should initialized Crispy Beef with a price of 5.00' do
+      expect(Dish.crispy_beef.price).to eq(5.00)
+      end
+      
+    end
+
+end
