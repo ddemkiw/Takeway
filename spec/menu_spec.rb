e39c2a3fa3d207211d10da8c1c55c92e1967b02e
Danielle Demkiw <ddemkiw@gmail.com>
2014-12-30 11:42:46 +0000

    Changing menu_spec to match changes made to menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -5,11 +5,17 @@ describe Menu do
   let(:menu) {Menu.new}
   
   it 'should know the available dishes and their prices' do
-    expect(menu.list).to be_a Array
+    expect(menu.dishes).to be_a Hash
   end
 
   it 'should include peking duck' do
-    expect(menu.list).to include ({"peking_duck" => 3})
+    expect(menu.dishes).to include ({"peking_duck" => 3})
+  end
+
+  it 'should be able to update with new items' do
+    expect(menu.dishes.length).to eq(3)
+    menu.add("ginger beef", 5)
+    expect(menu.dishes.length).to eq(4)
   end
 
 end
