27e7568fc49f114645ff35c9e358f7c8acd5a01b
Danielle Demkiw <ddemkiw@gmail.com>
2015-01-05 15:21:04 +0000

    Adding Gemfile

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,7 +1,7 @@
-require_relative 'menu'
-require_relative 'cart'
-require_relative 'dish'
-require_relative 'message'
+# require_relative 'menu'
+# require_relative 'cart'
+# require_relative 'dish'
+# require_relative 'message'
 
 class Restaurant
 
@@ -10,51 +10,51 @@ attr_reader :cart
   def initialize
     @menu = Menu.new
     @cart = Cart.new
-    intro
   end 
 
-  def intro 
-    puts "___Menu___"
-    @menu.display
-    puts "please select an item from the menu"
-    puts "when you have completed your order, please type finish"
-    print">"
-    get_order
-  end
+#   def intro 
+#     puts "___Menu___"
+#     @menu.display
+#     puts "please select an item from the menu"
+#     puts "when you have completed your order, please type finish"
+#     print">"
+#     get_order
+#   end
 
-  def get_order
-    while(item = gets.chomp) do
-        puts item=="finish" ? finish : order(item)
-    end
-  end
+#   def get_order
+#     while(item = gets.chomp) do
+#         puts item=="finish" ? finish : order(item)
+#     end
+#   end
 
-  def confirm_order
-    puts "Items #{@cart.list_items}"
-    puts "Total : £#{@cart.total_price}"
-    puts "Please enter payment amount"
-    print "> £"
-      get_payment
-      send_confirmation
-      quit
-  end
+#   def confirm_order
+#     puts "Items #{@cart.list_items}"
+#     puts "Total : £#{@cart.total_price}"
+#     puts "Please enter payment amount"
+#     print "> £"
+#       get_payment
+#       send_confirmation
+#       quit
+#   end
 
-    def get_payment
-     payment = gets.chomp.to_i
-      until payment == @cart.total_price do 
-        puts  "sorry, you did not enter the correct amount. please try again"
-        print "> £"
-        payment = gets.chomp.to_i
-      end
-  end
+#   def get_payment(payment)
 
-  def send_confirmation
-    message = Message.new
-    message.send_message("+447730344711")
-    puts "Thank you for your order. A text message has been sent to your phone"
-  end
+#     payment = gets.chomp.to_i
+#       until payment == @cart.total_price do 
+#         puts  "sorry, you did not enter the correct amount. please try again"
+#         print "> £"
+#         payment = gets.chomp.to_i
+#       end
+#   end
 
-  def new_order(item)
-    new_order = @menu.dishes.assoc(item)
+#   def send_confirmation
+#     message = Message.new
+#     message.send_message("+447730344711")
+#     puts "Thank you for your order. A text message has been sent to your phone"
+#   end
+
+  def new_order(menu_key)
+    new_order = @menu.dishes.assoc(menu_key)
     @name = new_order[0]  
     @price = new_order[1]
   end
@@ -65,18 +65,18 @@ attr_reader :cart
     @cart.add(dish)
   end
 
-  # def cancel(item)
-  #   cart.items.delete_if {|dish| dish.name == item}
-  # end
+#   # def cancel(item)
+#   #   cart.items.delete_if {|dish| dish.name == item}
+#   # end
 
-  def finish
-    confirm_order
-  end
+#   def finish
+#     confirm_order
+#   end
 
-  def quit
-    cart.empty
-    exit!
-  end
+#   def quit
+#     cart.empty
+#     exit!
+#   end
 
 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,4 +1,5 @@
 require 'restaurant'
+require 'stringio'
 
 describe Restaurant do
 
@@ -6,12 +7,12 @@ describe Restaurant do
   let(:message) {double :message}
 
 
-  before do
-    Message.any_instance.stub(:send_message)
-    Restaurant.any_instance.stub(:puts)
-    Restaurant.any_instance.stub(:intro)
-    Restaurant.any_instance.stub(:get_payment)
-  end
+  # before do
+  #   Message.any_instance.stub(:send_message)
+  #   # Restaurant.any_instance.stub(:puts)
+  #   # Restaurant.any_instance.stub(:print)
+  #   Restaurant.any_instance.stub(:intro)
+  # end
 
     it 'should be able to add order to cart' do 
       restaurant.order("noodles")
@@ -20,16 +21,39 @@ describe Restaurant do
       expect(restaurant.cart.items_count).to eq(3) 
     end
 
-    it 'should be able to create a dish object from an order' do 
-      restaurant.new_order('noodles')
-      expect(dish.name).to eq('noodles')
-    end
+    it 'should be able to take payment' do 
 
-    it 'should be able to confirm order' do 
-      expect(restaurant).to receive(:intro).with no_args
-      restaurant.order('noodles')
-      expect(restaurant.confirm_order).to 
+
     end 
+
+
+
+    # it 'should be able to create a dish object from an order' do 
+    #   restaurant.order('noodles')
+    #   restaurant.order('peking_duck')
+    #   p restaurant
+    #   expect(cart.items).to include?('noodles')
+    # end
+
+    # it 'should be able to confirm order' do 
+    #   Restaurant.stub(:get_payment)
+    #   Restaurant.stub(:send_confirmation)
+    #   restaurant.order('noodles')
+    #   restaurant.confirm_order
+    #   # expect(STDOUT).to receive(:puts).with("Please enter payment amount")
+    #   # allow(STDIN).to receive(:gets) { '2\n' }
+    # end
+
+    # it 'should be able to take payment' do
+    #   restaurant.order('noodles')
+    #   restaurant.get_payment
+    #   allow(STDIN).to receive(:gets) { '9 \n' }
+    #   expect(STDOUT).to receive(:puts).with("sorry, you did not enter the correct amount. please try again")
+    # end
+
+  
+     
+
 
     # it 'should be able to cancel and order' do 
     #   restaurant.order("chow_mein")
